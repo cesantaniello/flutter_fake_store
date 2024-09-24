20145270e525989fa4a9b7bbbf16c6144b7ffbab
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2024-09-24 17:45:09 +0200

    Implement appBar in Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+import '../widget/appbar_icons.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,8 +17,24 @@ class _HomeScreenState extends State<HomeScreen> {
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: const Scaffold(
-        body: Center(child: Text("Welcome to this course")),
+      child: Scaffold(
+        appBar: AppBar(
+          // elevation: 4,
+          title: const Text('Home'),
+          leading: AppBarIcons(
+            function: () {},
+            icon: IconlyBold.category,
+          ),
+          actions: [
+            AppBarIcons(
+              function: () {},
+              icon: IconlyBold.user3,
+            ),
+          ],
+        ),
+        body: const Center(
+          child: Text("Welcome to Home Screen"),
+        ),
       ),
     );
   }
