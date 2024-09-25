dca1ed21fc8aa4bf2cb60a4f2f62e3b560598cd5
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2024-09-25 17:07:07 +0200

    Implement Swiper widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:store_api_flutter_course/consts/global_colors.dart';
@@ -19,13 +20,16 @@ class _HomeScreenState extends State<HomeScreen> {
     _textEditingController = TextEditingController();
     super.initState();
   }
+
   @override
   void dispose() {
     _textEditingController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
@@ -54,28 +58,40 @@ class _HomeScreenState extends State<HomeScreen> {
                 controller: _textEditingController,
                 keyboardType: TextInputType.text,
                 decoration: InputDecoration(
-                  hintText: "Search", 
-                  filled: true,
-                  fillColor: Theme.of(context).cardColor,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide:
-                      BorderSide(color: Theme.of(context).cardColor),
+                    hintText: "Search",
+                    filled: true,
+                    fillColor: Theme.of(context).cardColor,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide:
+                          BorderSide(color: Theme.of(context).cardColor),
                     ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide(
-                      width: 1,
-                      color: Theme.of(context).colorScheme.secondary
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.secondary),
                     ),
+                    suffixIcon: Icon(
+                      IconlyLight.search,
+                      color: lightIconsColor,
+                    )),
+              ),
+              SizedBox(
+                height: size.height * 0.25,
+                child: Swiper(
+                  itemCount: 3,
+                  itemBuilder: (ctx, index) => const SaleWidget(),
+                  pagination: const SwiperPagination(
+                    alignment: Alignment.bottomCenter,
+                    builder: DotSwiperPaginationBuilder(
+                      color: Colors.white,
+                      activeColor: Colors.red
+                    )
                   ),
-                  suffixIcon: Icon(
-                    IconlyLight.search,
-                    color: lightIconsColor,
-                  )
+                  //control: const SwiperControl(),
                 ),
               ),
-              const SaleWidget(),
             ],
           ),
         ),
