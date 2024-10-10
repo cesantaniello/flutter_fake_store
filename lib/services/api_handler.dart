4d279d49e93ea5b17e8c3469543ee8842a5a11b5
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2024-10-10 18:04:21 +0200

    Fetch and display users on screen

--- lib/services/api_handler.dart
+++ lib/services/api_handler.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:store_api_flutter_course/consts/api_consts.dart';
 import 'package:store_api_flutter_course/models/categories_model.dart';
 import 'package:store_api_flutter_course/models/products_model.dart';
+import 'package:store_api_flutter_course/models/users_model.dart';
 
 class APIHandler {
   static Future<List<dynamic>> getData({required String target}) async {
@@ -31,5 +32,10 @@ class APIHandler {
   static Future<List<CategoriesModel>> getAllCategories() async {
     List temp = await getData(target: "categories");
     return CategoriesModel.categoriesFromSnapshot(temp);
+  }
+
+  static Future<List<UsersModel>> getAllUsers() async {
+    List temp = await getData(target: "users");
+    return UsersModel.usersFromSnapshot(temp);
   }
 }
